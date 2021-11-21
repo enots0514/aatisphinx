d19a3996ef99e2546a2c14d46653be274bfe22ad
enots0514 <aattitude@daum.net>
2021-11-21 21:48:23 +0900

    bcript모듈 사용해 암호화한 로그인 포맷

--- controllers/registerCtr.js
+++ controllers/registerCtr.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// const bcrypt = require('bcrypt');
 const Guest = require('../models/guestSchema');
 
 
@@ -8,20 +8,37 @@ module.exports.login = (req, res) => {
         Guest.findOne({email: req.body.email})
              .then( user => { 
                                if(user){
-                                        if(user.pwd == req.body.pwd){
-                                            console.log("로그인 되었습니다.");
-                                            res.render('home/index', {guest_nicname:`${user.nicname}님 로그인 되었습니다`}); 
-                                        }else{
-                                            console.log("비번이 틀렸습니다.") ;
-                                            res.render('register/login', {formTitle:"비번이 틀렸습니다"});
+
+                                    user.comparePwd(req.body.pwd, function(err, isMatch) {
+                                        if(err) {
+                                            console.log("로그인 중 에러 발생함.");
+                                            next(err);
                                         }
+                                        else if(null, !isMatch){
+                                            console.log("비밀번호가 틀립니다.");
+                                            res.render('register/login', {formTitle:"비밀번호가 틀립니다."});
+                                            return;
+                                        }
+                                        console.log("로그인 되었습니다.");
+                                         res.render('home/index', {guest_nicname:`${user.nicname}님 로그인 되었습니다`}); 
+                                     }) 
+
+
+
+                                        // if(user.pwd == req.body.pwd){
+                                        //     console.log("로그인 되었습니다.");
+                                        //     res.render('home/index', {guest_nicname:`${user.nicname}님 로그인 되었습니다`}); 
+                                        // }else{
+                                        //     console.log("비번이 틀렸습니다.") ;
+                                        //     res.render('register/login', {formTitle:"비번이 틀렸습니다"});
+                                        // }
                                 } else{
                                     console.log("유저 정보가 없습니다.");
                                      res.render('register/login', {formTitle:"유저 정보가 없습니다."});
                                 } 
              })
-             .catch( error => {
-                console.log(`로그인 중 에러 발생함: ${error}`);
+             .catch( err => {
+                console.log(`로그인 중 에러 발생함: ${err}`);
              })
  };
 
--- models/guestSchema.js
+++ models/guestSchema.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 // 스키마 생성
@@ -10,6 +13,42 @@ const guestSchema = new mongoose.Schema({
 });
 
 
+
+guestSchema.pre('save', function(next) {
+     const guest = this; // guestSchema를 가르킨다.
+
+  if(guest.isModified('pwd') ||guest.isNew) {
+    bcrypt.genSalt(saltRounds, function(err, salt){
+      if(err) return next(err);
+ 
+ 
+      bcrypt.hash(guest.pwd, salt, function(err, hash) {
+        
+          if(err) {
+              console.log("비밀번호 해시과정 중에 에러가 발생했습니다.");
+              return next(err);
+          } else {
+            guest.pwd = hash;
+            next()
+          }
+       }) 
+     })
+  } else {
+    next();
+  }
+      
+});
+
+guestSchema.methods.comparePwd = function(pwd, cb) {
+
+    bcrypt.compare(pwd, this.pwd, function(err, isMatch) {
+      if(err) {
+        return cb(err);
+       } else {
+         cb(null, isMatch)
+       }
+   })
+}
 
 
 module.exports = mongoose.model('guest', guestSchema);
